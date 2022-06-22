1e5b453cd53a602d6075e2f0d5fd6c2c01401991
PyHbl4 <maxim_runkov@mail.ru>
2022-06-22 13:46:07 +0500

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    let arr = n.toString().split('');
+    let result = '';
+    for (let i = (arr.length - 1); i >= 0; i--) {
+        result = `${result}${arr[i]}`;
+    }
+    return +result;
 }
