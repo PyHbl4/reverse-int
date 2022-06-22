428df44dadfa9d3472f0625767fb8c7173fed3c7
PyHbl4 <maxim_runkov@mail.ru>
2022-06-22 14:13:35 +0500

    fix: add math.abs to number

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 module.exports = function reverse (n) {
-    let arr = n.toString().split('');
+    let arr = Math.abs(n).toString().split('');
     let result = '';
     for (let i = (arr.length - 1); i >= 0; i--) {
         result = `${result}${arr[i]}`;
